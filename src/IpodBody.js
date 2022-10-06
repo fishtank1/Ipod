dc12cef701c3ba6b02a17c8fe51b530b93415b47
unknown <senguptasaumyajeet@outlook.com>
2022-10-06 20:36:54 +0530

    third commit

--- src/IpodBody.js
+++ src/IpodBody.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import './IpodBody.css'
 
 class IpodBody extends Component {
+
     render() {
         return (
             <div className="Ipod-container">
--- src/IpodWheel.js
+++ src/IpodWheel.js
@@ -6,15 +6,47 @@ import ZingTouch from "zingtouch";
 
 class IpodWheel extends Component {  
   componentDidMount() {
+    var indx = 0, array = document.querySelectorAll('.tiles');
     var currentAngle = 0;
     var target = document.querySelector('.wheel-inner-div');
     var region = new ZingTouch.Region(target);
     document.querySelector('.wheel-outer-div').style.transform = 'rotate(0deg)';
     
     region.bind(target, 'rotate', function(e) {
+      var prev = Math.floor(currentAngle);
       currentAngle += e.detail.distanceFromLast;
-      var rotatable = currentAngle;
-      console.log(rotatable);
+      if(Math.floor(currentAngle) > 15) {
+        console.log(indx);
+        array[4].classList.remove('currentSelection');
+        if(indx != 0) {
+          array[indx-1].classList.remove('currentSelection');
+        }
+        currentAngle = 0;
+        array[indx].classList.add('currentSelection');
+        indx += 1;
+        if(indx == 5) {
+          indx = 0;
+        }
+      }
+      // if(Math.floor(currentAngle) > 15) {
+      //   array[indx].classList.remove('currentSelection');
+      //   if(indx != array.length-1) {
+      //     indx += 1;
+      //     currentAngle = 0;
+      //     array[indx].classList.add('currentSelection');
+      //   } else {
+      //     indx = 0;
+      //   }
+      // } else {
+      //   if(indx != 0) {
+      //     array[indx].classList.remove('currentSelection');
+      //     indx -= 1;
+      //     currentAngle = 0;
+      //     array[indx].classList.add('currentSelection');
+      //   } else {
+      //     indx = array.length-1;
+      //   }
+      // }
     });
   }
 
